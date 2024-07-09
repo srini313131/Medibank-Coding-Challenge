079d171f604f24d61ae26cfd1ed030b25992151f
srini313131 <52782561+srini313131@users.noreply.github.com>
2024-07-09 20:15:20 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Accordion, Card, Spinner } from "react-bootstrap";
 import { GenderEnum } from "./types";
 import { UseFetch } from "./useFetch";
 import { useRef } from 'react' 
-
+import './App.css';
 const URL =
   "https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json";
 function App() {
